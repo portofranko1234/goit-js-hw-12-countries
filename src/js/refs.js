9654e688512a3d0e9bee5c64c7bae2706deee6eb
portofranko1234 <75642806+portofranko1234@users.noreply.github.com>
2021-07-04 23:19:40 +0000

    Deploying to gh-pages from @ portofranko1234/goit-js-hw-12-countries@8007e6b042a803b6a6bdf051680a2cd39d3f4f2d 🚀

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-const refs = {
-  searchInput: document.querySelector('#js-search-input'),
-  countriesList: document.querySelector('.js-countries-list'),
-  countryContainer: document.querySelector('#js-country-container'),
-};
-
-export default refs;
